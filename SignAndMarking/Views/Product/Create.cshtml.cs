d98c8913daa11bcdb2a34c4666bcf68f0e25f27d
Kaniuka <kaniukaroma@gmail.com>
2024-03-26 00:13:49 +0200

    created a ProductController with the Create method

---
+++ SignAndMarking/Views/Product/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignAndMarking.Views.Product;
+
+public class CREATE : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ SignAndMarking/Views/Product/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignAndMarking.Views.Product;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
